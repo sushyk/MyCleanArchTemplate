3a1a434e91481ff98220dba8d77c76fe7425f660
Sushil <ksk.sushil.kumar@gmail.com>
2025-04-30 20:24:44 +0530

    add validator

---
+++ MyCleanArchTemplate.Application/Customers/CreateCustomer/CreateCustomerCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace MyCleanArchTemplate.Application.Customers.CreateCustomer;
+
+internal sealed class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+{
+    public CreateCustomerCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+
+        RuleFor(x => x.Email).NotEmpty().EmailAddress().MaximumLength(50);
+    }
+}
