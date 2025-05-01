614b6ac4b31c58fa1ff7c1083b9b326b866fca00
Sushil <ksk.sushil.kumar@gmail.com>
2025-05-01 08:16:10 +0530

    remove messaging abstractions

--- MyCleanArchTemplate.Application/Abstractions/Messaging/IQueryHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using MyCleanArchTemplate.Core.Shared;
-
-namespace MyCleanArchTemplate.Application.Abstractions.Messaging;
-
-public interface IRequestHandler<TQuery, TResponse> where TQuery : IQuery<TResponse>
-{
-    Task<Result<TResponse>> Handle(TQuery query, CancellationToken cancellationToken);
-}
