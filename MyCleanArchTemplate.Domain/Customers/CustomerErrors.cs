1b1b8085ad519b529373b3f64d14ac961c25e538
Sushil <ksk.sushil.kumar@gmail.com>
2025-05-01 12:21:02 +0530

    add email not unique error

--- MyCleanArchTemplate.Domain/Customers/CustomerErrors.cs
+++ MyCleanArchTemplate.Domain/Customers/CustomerErrors.cs
@@ -7,4 +7,8 @@ public static class CustomerErrors
     public static Error NotFound(long customerId) => Error.NotFound(
         "Customers.NotFound",
         $"The customer with the Id = {customerId} was not found");
+
+    public static Error EmailNotUnique(string email) => Error.Conflict(
+        "Customers.EmailNotUnique",
+        $"The provided email {email} is already in use.");
 }
